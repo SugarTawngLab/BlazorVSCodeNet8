91a9149756bc6844ff80e2665bdea251e215f6c6
SugarTawng <20521185@gm.uit.edu.vn>
2025-03-01 02:03:04 +0700

    first commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+namespace BlazorVSCodeNet8.Models{
+  public class ApplicationUser 
+ {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+  }
+}
